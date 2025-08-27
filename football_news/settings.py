cf8749ed6a26586442c68716781b9d8d6ec949d9
Hakim Nizami <hakimnizami15@gmail.com>
2025-08-27 13:54:31 +0700

    Deployment

--- football_news/settings.py
+++ football_news/settings.py
@@ -29,7 +29,7 @@ PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "muhamad-hakim41-footballnews.pbp.cs.ui.ac.id"]
 
 
 # Application definition
